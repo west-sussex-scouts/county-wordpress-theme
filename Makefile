f384c7d4353f00a8bc873ab82c7fcb3be2c1cba0
Tom Whitwell <tom@whi.tw>
2019-03-16 17:45:32 +0000

    restructure

--- Makefile
+++ Makefile
@@ -1,14 +1,10 @@
 
 dep: package.json
 	yarn install --dev
-	rm -rf scss/vendor/bootstrap || true
-	cp -a node_modules/bootstrap/scss scss/vendor/bootstrap
 
-build: index.scss scripts/header.js scripts/footer.js
-	node_modules/.bin/sass index.scss:index.css --style compressed
-	node_modules/.bin/webpack
+build: src/scss src/js/header.js src/js/footer.js
+	node_modules/.bin/sass src/scss/index.scss:assets/css/index.css --style compressed
+	node_modules/.bin/webpack --mode=production
 
-watch: index.scss
-	node_modules/.bin/sass --watch index.scss index.css
-
-minify:
+watch: src/scss
+	node_modules/.bin/sass --watch src/scss/index.scss:assets/css/index.css
--- index.php
+++ index.php
@@ -1,22 +1,16 @@
-<html lang="en">
-  <?php /* Template Name: Front Page */ ?>
-  <?php require 'template-parts/head/head.php';?>
-  <body>
-    <?php require 'template-parts/header/header.php';?>
-    <div class="content container pt-xl-4 pt-lg-4 pt-md-2 pt-sm-2 pt-1">
-      <div class="row">
+<?php get_header(); ?>
+<div class="content container pt-xl-4 pt-lg-4 pt-md-2 pt-sm-2 pt-1">
+    <div class="row">
         <div class="col-md">
-          <?php
-            if ( have_posts() ) : while ( have_posts() ) : the_post();
-              get_template_part( 'template-parts/content/content', 'single' );
-            endwhile;
-            else:
-              get_template_part( 'template-parts/content/content', 'single' );
-            endif;
-          ?>
+			<?php
+			if ( have_posts() ) : while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/content/content', 'single' );
+			endwhile;
+			else:
+				get_template_part( 'template-parts/content/content', 'single' );
+			endif;
+			?>
         </div>
-      </div>
     </div>
-    <?php get_footer(); ?>
-  </body>
-</html>
+</div>
+<?php get_footer(); ?>
