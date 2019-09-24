9753dd0a390f6e3b33217b3190e6da087b5c51ec
Tom Whitwell <tom@whi.tw>
2019-09-24 21:42:53 +0100

    Actually add the screenshot when packaging

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-zip -r9 county-wordpress-theme.zip *.php LICENSE.MD style.css vendor/ assets/ template-parts/
+zip -r9 county-wordpress-theme.zip *.php LICENSE.MD style.css screenshot.png vendor/ assets/ template-parts/
 sha256sum county-wordpress-theme.zip > county-wordpress-theme.zip.sha256
