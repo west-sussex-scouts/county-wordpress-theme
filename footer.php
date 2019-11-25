9f2b67c1396c17a697ac44233ba772c132bb56e9
Tom Whitwell <tom@whi.tw>
2019-11-25 13:51:14 +0000

    Start of minor rebrand
    Remove the Top banner, pending replacement, restyle footer to match scouts.org.uk brand

--- footer.php
+++ footer.php
@@ -27,26 +27,26 @@
 				}
 			}
 			?>
-            <div class="col col-6 col-md-3 scouts-purple footer-box pt-2 pb-2 pl-2 pr-2 pt-md-5 pb-md-5 pl-md-4 pr-md-4">
+            <div class="col col-12 col-md-3 col footer-box pt-2 pb-2 pl-2 pr-2 pt-md-5 pb-md-5 pl-md-4 pr-md-4">
                 <h6><?php echo $menu_list[0]['name'] ?></h6>
 				<?php echo $menu_list[0]['content'] ?>
                 <!-- <a href="https://www.scouts.org.uk/" class="footer-link"><p>Official Scouts Website</p></a> -->
             </div>
-            <div class="col col-6 col-md-3 scouts-blue footer-box pt-2 pb-2 pl-2 pr-2 pt-md-5 pb-md-5 pl-md-4 pr-md-4">
+            <div class="col col-6 col-md-3 footer-box pt-2 pb-2 pl-2 pr-2 pt-md-5 pb-md-5 pl-md-4 pr-md-4">
                 <h6><?php echo $menu_list[1]['name'] ?></h6>
 				<?php echo $menu_list[1]['content'] ?>
                 <!-- <a href="https://members.scouts.org.uk/supportresources/search/?cat=299,304">Young People First: Safeguarding</a>
 				<br /><a href="https://worthing-scouts.org.uk/district/privacy-data-protection/">Privacy &amp; Data Protection</a>
 				<br /><a href="https://worthing-scouts.org.uk/scout-shop/">Scout Shop</a> -->
             </div>
-            <div class="col col-6 col-md-3 scouts-green pt-2 pb-2 pl-2 pr-2 pt-md-5 pb-md-5 pl-md-4 pr-md-4">
+            <div class="col col-6 col-md-3 pt-2 pb-2 pl-2 pr-2 pt-md-5 pb-md-5 pl-md-4 pr-md-4">
                 <h6><?php echo $menu_list[2]['name'] ?></h6>
 				<?php echo $menu_list[2]['content'] ?>
                 <!-- <a href="https://worthing-scouts.org.uk/get-involved/">Get Involved</a>
 				<br /><a href="https://worthing-scouts.org.uk/get-involved/volunteer/">Volunteer</a>
 				<br /><a href="https://worthing-scouts.org.uk/contact-us/">Contact Us</a> -->
             </div>
-            <div class="col col-6 col-md-3 scouts-red pt-2 pb-2 pl-2 pr-2 pt-md-5 pb-md-5 pl-md-4 pr-md-4">
+            <div class="col col-6 col-md-3 pt-2 pb-2 pl-2 pr-2 pt-md-5 pb-md-5 pl-md-4 pr-md-4">
                 <h6>Contact Us</h6>
                 <span>&copy; <?php echo get_bloginfo( 'name', 'raw' ) ?> <?php echo date( 'Y' ) ?><br/><?php if ( get_option( 'charity_number' ) ): ?> Registered Charity Number
                         <span id="charity_number"><?php echo get_option( 'charity_number' ) ?></span><?php endif ?></span>
--- front-page.php
+++ front-page.php
@@ -1,27 +1,27 @@
 <?php /* Template Name: Front Page */ ?>
 <?php get_header(); ?>
-<div class="container-flex">
-    <div class="row top-banner">
-        <img width="2048" height="945"
-             src="<?php echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-2048.jpg'; ?>"
-             class="img-fluid" alt=""
-             srcset="<?php echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-2048.jpg'; ?> 2048w, <?php echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-300.jpg'; ?> 300w, <?php echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-768.jpg'; ?> 768w, <?php echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-1024.jpg'; ?> 1024w, <?php echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-1000.jpg'; ?> 1000w, <?php echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-500.jpg'; ?> 500w, <?php echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-600.jpg'; ?> 600w"
-             sizes="(max-width: 2048px) 100vw, 2048px">
-        <div class="col-md-6 offset-md-1 banner-overlay bg-scouts-purple pt-2 pb-2 pl-4 pr-4 pt-md-5 pb-md-3 pl-md-5 pr-md-5 rounded"
-             id="top-banner-overlay">
-            <h2>Skills for Life</h2>
-            <p>As Scouts, we believe in preparing young people with Skills for Life. We encourage young people to do
-                more, learn more and be more. Each week, we help over 460,000 young people across the UK enjoy fun and
-                adventure while developing the skills they need to succeed, now and in the future. </p>
-            <div class="btn_row">
-                <a href="/?page_id=<?= get_option( "sfl_fom_target" ) ?>" id="sfl_fom"
-                   class="btn btn-big btn-big-border btn-scouts-purple">Find out more</a>
-                <a href="/?page_id=<?= get_option( "sfl_jt_target" ) ?>" id="sfl_jt"
-                   class="btn btn-big btn-scouts-white-teal">Join today</a>
-            </div>
-        </div>
-    </div>
-</div>
+<!--<div class="container-flex">-->
+<!--    <div class="row top-banner">-->
+<!--        <img width="2048" height="945"-->
+<!--             src="--><?php //echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-2048.jpg'; ?><!--"-->
+<!--             class="img-fluid" alt=""-->
+<!--             srcset="--><?php //echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-2048.jpg'; ?><!-- 2048w, --><?php //echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-300.jpg'; ?><!-- 300w, --><?php //echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-768.jpg'; ?><!-- 768w, --><?php //echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-1024.jpg'; ?><!-- 1024w, --><?php //echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-1000.jpg'; ?><!-- 1000w, --><?php //echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-500.jpg'; ?><!-- 500w, --><?php //echo get_template_directory_uri() . '/assets/images/stock/2-scouts-canoeing-600.jpg'; ?><!-- 600w"-->
+<!--             sizes="(max-width: 2048px) 100vw, 2048px">-->
+<!--        <div class="col-md-6 offset-md-1 banner-overlay bg-scouts-purple pt-2 pb-2 pl-4 pr-4 pt-md-5 pb-md-3 pl-md-5 pr-md-5 rounded"-->
+<!--             id="top-banner-overlay">-->
+<!--            <h2>Skills for Life</h2>-->
+<!--            <p>As Scouts, we believe in preparing young people with Skills for Life. We encourage young people to do-->
+<!--                more, learn more and be more. Each week, we help over 460,000 young people across the UK enjoy fun and-->
+<!--                adventure while developing the skills they need to succeed, now and in the future. </p>-->
+<!--            <div class="btn_row">-->
+<!--                <a href="/?page_id=--><?//= get_option( "sfl_fom_target" ) ?><!--" id="sfl_fom"-->
+<!--                   class="btn btn-big btn-big-border btn-scouts-purple">Find out more</a>-->
+<!--                <a href="/?page_id=--><?//= get_option( "sfl_jt_target" ) ?><!--" id="sfl_jt"-->
+<!--                   class="btn btn-big btn-scouts-white-teal">Join today</a>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
 <div class="container welcome-section pt-4 pb-3">
     <div class="row">
         <div class="col-md-7 col-sm-12">
